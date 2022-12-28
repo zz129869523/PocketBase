f3235aeeff4d4389db59779dd121943415d1348c
zz129869523 <zz129869523@gmail.com>
2022-12-29 01:59:30 +0800

    fix Utils

--- Sources/PocketBase/Utils/Utils.swift
+++ Sources/PocketBase/Utils/Utils.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 public class Utils {
-  static func dictionaryToStruct<T: Codable>(dictionary: [String: Any]) throws -> T {
+  public static func dictionaryToStruct<T: Codable>(dictionary: [String: Any]) throws -> T {
     return try JSONDecoder().decode(T.self, from: JSONSerialization.data(withJSONObject: dictionary))
   }
   
-  static func stringToDictionary(text: String) -> [String: Any]? {
+  public static func stringToDictionary(text: String) -> [String: Any]? {
     if let data = text.data(using: .utf8) {
       do {
         return try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
