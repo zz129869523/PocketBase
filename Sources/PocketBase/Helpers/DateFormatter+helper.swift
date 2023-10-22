70ce271406e99899d15f9ca37c7607b483a48411
sergeypdev <sergeypoznyak@protonmail.com>
2023-10-22 18:27:33 +0400

    Fix date formatter encoding
    This allows using the same date formatter in query filters.
    
    Example:
    ```swift
    await pocketbase.collection("table")
      .getFullList(
        filter: "updated > '\(DateFormatter.defaultPocketbase.string(from: messageCursor))'"
      )
    ```
    
    Previously local timezone would be appended to the date, which parses
    wrong in pocketbase.

--- Sources/PocketBase/Helpers/DateFormatter+helper.swift
+++ Sources/PocketBase/Helpers/DateFormatter+helper.swift
@@ -11,8 +11,9 @@ public extension DateFormatter {
   /// Default date formatting used by PocketBase server.
   static let defaultPocketBase: DateFormatter = {
     let formatter = DateFormatter()
-    formatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSSZ"
+    formatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSSX"
     formatter.locale = Locale(identifier: "en_US_POSIX")
+    formatter.timeZone = .init(secondsFromGMT: 0)
     formatter.calendar = Calendar(identifier: .iso8601)
     return formatter
   }()
